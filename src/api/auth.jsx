7512217cde8808a9b5942bca7fee3f667d29f4ce
chandan-002 <chandan.unicoglobal@gmail.com>
2023-02-16 13:14:08 +0530

    done

--- src/api/auth.jsx
+++ src/api/auth.jsx
@@ -36,6 +36,47 @@ const login = async (userData) => {
     })
 };
 
+
+const Register = async (userData) => {
+    return new Promise(function (resolve, reject) {
+        Axios.post("/admin/user", userData).then((res) => {
+            if (!res.data.success) {
+                Toast({
+                    message: res.data.message,
+                    type: 'error'
+                });
+                reject({
+                    data: res.data,
+                    other: res,
+                    success: false
+                });
+            };
+            Toast({
+                message: "Success ! Please verify email before login",
+                type: 'info'
+            });
+            resolve({
+                data: res.data,
+                other: res,
+                success: true
+            });
+        }).catch((err) => {
+            if (err.response?.data) {
+                Toast({
+                    message: `Error from Register` ?? err.response.data.message,
+                    type: 'error'
+                });
+            };
+            reject({
+                data: err.response?.data,
+                other: err,
+                success: false
+            });
+        })
+    })
+};
+
 export {
-    login
+    login,
+    Register
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,26 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import Input from '../components/Input';
 import { Logo } from '../assets/svg';
 import Button from '../components/Button';
 
+import { Register } from '../api/auth';
+import Toast from '../utils/Toast';
+
 function CreateAccount() {
+  const [userData, setUserData] = useState({ fullname: "", email: '', confirmpassword: '', password: '', mobile: "", role: "admin", height: "", weight: "" });
+  const history = useHistory();
+  const [loading, setLoading] = useState(false)
 
   const handleRegister = () => {
-
+    setLoading(true)
+    if (userData?.confirmpassword === userData?.password) {
+      delete userData.confirmpassword;
+      Register(userData).then(res => {
+        setLoading(false)
+        history.push('/app/home');
+      }).catch(err => {
+        setLoading(false)
+      })
+    } else {
+      Toast({
+        message: "Password not same",
+        type: 'error'
+      })
+    }
   }
 
   return (
     <div className="flex flex-col justify-center items-center">
       <Logo className={"h-64 w-72"} />
       <div className='flex flex-col gap-2 w-full md:w-[450px] px-5 md:px-0'>
-        <Input label="Email" placeholder={"example@gmail.com"} />
-        <Input label="Password" placeholder={"*********"} />
-        <Input label="Confirm Password" placeholder={"*********"} />
-        <Input label="Height - Optional" placeholder={"5.7"} />
-        <Input label="Weight - Optional" placeholder={"55"} />
+        <Input name="fullname" setInput={setUserData} label="Fullname" placeholder={"Jhon Doe"} />
+        <Input name="mobile" setInput={setUserData} label="Mobile" placeholder={"9911223311"} />
+        <Input name="email" setInput={setUserData} label="Email" placeholder={"example@gmail.com"} />
+        <Input name="password" setInput={setUserData} label="Password" placeholder={"*********"} />
+        <Input name="confirmpassword" setInput={setUserData} label="Confirm Password" placeholder={"*********"} />
+        <Input name="height" setInput={setUserData} label="Height - Optional" placeholder={"5.7"} />
+        <Input name="weight" setInput={setUserData} label="Weight - Optional" placeholder={"55"} />
         <p className='text-base cursor-pointer text-green-600 text-left'>By creating an account you are agreeing to our terms & conditions.</p>
-        <Button text={'Register'} onClick={handleRegister} />
+        <Button text={!loading ? 'Register' : "Please wait"} onClick={handleRegister} />
         <Link to='/auth/login'>
           <p className='text-base cursor-pointer text-center text-green-600 hover:underline'>Already an account? Sign in.</p>
         </Link>
