0387d5c29685cccc427021e5337db8f3632f7e91
chandan-002 <chandan.unicoglobal@gmail.com>
2023-02-18 19:50:37 +0530

    new api url

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 // Setting up base Url for fetching data
 const Axios = axios.create({
-    baseURL: "https://myworkoutfriend-backend-production.up.railway.app/api/v1/",
+    // baseURL: "https://myworkoutfriend-backend-production.up.railway.app/api/v1/",
+    baseURL: "https://api.myworkoutfriend.in/api/v1/",
     // baseURL: "http://localhost:9000/api/v1/",
     headers: {
         "Content-Type": "application/json",
