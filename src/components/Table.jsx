87d25cc6fa641baa952f803b369c7b01905abe19
chandan-002 <chandan.unicoglobal@gmail.com>
2023-02-19 12:11:08 +0530

    fixed width issue

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -21,17 +21,17 @@ const Table = ({ heading, data, weight, }) => {
             {
                 data?.length > 0 ? data?.map(item => {
                     return (
-                        <div className="border-2 border-gray-200 bg-gray-50 py-4 px-6 rounded shadow-md md:w-[800px] w-full">
+                        <div className="border-2 border-gray-200 bg-gray-50 py-4 px-3 md:px-6 rounded shadow-md md:w-[800px] w-full">
                             <div className="grid grid-cols-3 justify-between py-2 pb-4">
                                 <h1 className="font-bold md:text-xl flex flex-col col-span-1"><span className="text-xs font-normal text-orange-500">Exercise</span> {item?.exerciseid?.name} </h1>
                                 <h1 className="font-bold md:text-xl flex flex-col col-span-1"> <span className="text-xs font-normal text-orange-500">Category</span>{item?.categoryid?.name} </h1>
                                 <h1 className="font-bold md:text-xl flex flex-col col-span-1"> <span className="text-xs font-normal text-orange-500">Date</span>{moment(item?.createdAt).format('DD MMM YYYY')}</h1>
                             </div>
-                            <table className="w-full text-sm text-left text-gray-500 border-[1px]">
-                                <thead className="text-xs text-gray-700 uppercase bg-orange-500">
+                            <table className="text-xs md:text-base text-left text-gray-500 border-[1px] w-full">
+                                <thead className=" text-gray-700 uppercase bg-orange-500">
                                     <tr>
                                         {
-                                            ["Set", "Reps", "Weight", "RPE", "X"]?.map((itm, i) => <th key={i} scope="col" className="px-6 py-3 text-white">
+                                            ["Set", "Reps", "Weight", "RPE", "X"]?.map((itm, i) => <th key={i} scope="col" className="px-5 md:px-6 py-3 text-white">
                                                 {itm}
                                             </th>)
                                         }
@@ -68,6 +68,7 @@ const Table = ({ heading, data, weight, }) => {
 
                                 </tbody>
                             </table>
+
                         </div>
                     )
                 }) : <div><ExclamationCircleIcon className="text-orange-500" /><p className="text-base">No Data Found</p></div>
