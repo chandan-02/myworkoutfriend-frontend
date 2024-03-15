f3903e4af01f2812e49d7547d274281edcdc9528
Chandan y <chandan.yadav@datatobiz.com>
2024-03-15 16:50:42 +0530

    sdfsdf

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,13 +1,15 @@
-import axios from "axios";
+import axios from 'axios'
 // Setting up base Url for fetching data
 const Axios = axios.create({
-    // baseURL: "https://myworkoutfriend-backend-production.up.railway.app/api/v1/",
-    // baseURL: "https://api.myworkoutfriend.in/api/v1/",
-    baseURL: "https://myworkoutfriend-backend.onrender.com/api/v1/",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        Authorization: `Bearer ${typeof window !== 'undefined' && localStorage.getItem('token')}`,
-    },
-});
-export default Axios;
+  // baseURL: "https://myworkoutfriend-backend-production.up.railway.app/api/v1/",
+  // baseURL: "https://api.myworkoutfriend.in/api/v1/",
+  baseURL: 'https://myworkoutfriend-backend.onrender.com/api/v1/',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    Authorization: `Bearer ${
+      typeof window !== 'undefined' && localStorage.getItem('token')
+    }`,
+  },
+})
+export default Axios
